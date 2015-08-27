0d2ae0b635a6b2862a08e1f63e995a24d8bbb53b
Adonis <sinoda2991@gmail.com>
2015-08-27 11:30:01 -0600

    plazas final

---
+++ SACAAE/Scripts/SiteScripts/plazaIndex.js
@@ -0,0 +1,12 @@
+﻿var tempID = 0;
+
+function initDelete(n) {
+    $('#codeLabel').text("Codigo: " + $("#Code_" + n).val());
+    tempID = n;
+}
+
+function deletePlaza() {
+    var deleteRoute = '/Plazas/Delete/' + tempID;
+    $('#plazaForm').attr('action', deleteRoute);
+    $('#plazaForm').submit();
+}
