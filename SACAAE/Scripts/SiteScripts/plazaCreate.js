315cddea25c38dfe019e5fcda04fec1405beaeb6
Adonis <sinoda2991@gmail.com>
2015-08-30 14:48:37 -0600

    plazas validation

---
+++ SACAAE/Scripts/SiteScripts/plazaCreate.js
@@ -0,0 +1,44 @@
+﻿// <author> Adonis Mora Angulo </author>
+// Disable submit button for default
+document.getElementById("createBtn").disabled = true;
+// Add events to validate inputs
+document.getElementById("TotalHours").oninput = function () { validate() };
+document.getElementById("EffectiveTime").oninput = function () { validate() };
+
+function validate() {
+    if (hoursValidate() && effectiveTimeValidate() && codeValidate())
+        document.getElementById("createBtn").disabled = false;
+    else
+        document.getElementById("createBtn").disabled = true;
+}
+
+function codeValidate() {
+    var code = $("#Code").val();
+    if (code != "")
+        return true;
+    return false;
+}
+
+function hoursValidate () {
+    var hours = $("#TotalHours").val();
+    if (numberValidate(hours)) 
+        if (hours <= 40) 
+            return true;
+    return false;
+};
+
+function effectiveTimeValidate() {
+    var hours = $("#EffectiveTime").val();
+    if (numberValidate(hours)) 
+        return true;
+    return false;
+};
+
+// Regex for positive number
+function numberValidate(num) {
+    var regex = /^[0-9]+$/;
+    if (regex.test(num)) {
+        return true;
+    }
+    return false;
+}
