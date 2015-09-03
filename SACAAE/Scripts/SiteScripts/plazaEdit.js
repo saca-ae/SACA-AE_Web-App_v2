a73b7af27ebd102f97a426ee281890a7c9de6064
Adonis <sinoda2991@gmail.com>
2015-09-03 13:47:19 -0600

    pedit

---
+++ SACAAE/Scripts/SiteScripts/plazaEdit.js
@@ -0,0 +1,43 @@
+﻿// <author> Adonis Mora Angulo </author>
+// Add events to validate inputs
+document.getElementById("Code").oninput = function () { validate() };
+document.getElementById("TotalHours").oninput = function () { validate() };
+document.getElementById("EffectiveTime").oninput = function () { validate() };
+
+function validate() {
+    if (hoursValidate() && effectiveTimeValidate() && codeValidate())
+        document.getElementById("editBtn").disabled = false;
+    else
+        document.getElementById("editBtn").disabled = true;
+}
+
+function codeValidate() {
+    var code = $("#Code").val();
+    if (code != "")
+        return true;
+    return false;
+}
+
+function hoursValidate() {
+    var hours = $("#TotalHours").val();
+    if (numberValidate(hours))
+        if (hours <= 40)
+            return true;
+    return false;
+};
+
+function effectiveTimeValidate() {
+    var hours = $("#EffectiveTime").val();
+    if (numberValidate(hours))
+        return true;
+    return false;
+};
+
+// Regex for positive number
+function numberValidate(num) {
+    var regex = /^[0-9]+$/;
+    if (regex.test(num)) {
+        return true;
+    }
+    return false;
+}
