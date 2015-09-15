6bd0f73b97cce70f7bb5fb916c3a6f665f107688
estebansegura17031992 <esteban.1703@gmail.com>
2015-09-14 20:44:40 -0600

    sync

--- SACAAE/Scripts/SiteScripts/ComissionProfessor.js
+++ SACAAE/Scripts/SiteScripts/ComissionProfessor.js
@@ -1,9 +1,34 @@
-﻿var contador = 2;
+﻿$(document).ready(function () {
+
+
+    $("#buttonPost").on("click", function () {
+        var listName = "ScheduleCommission";
+
+        var i = 0;
+        $("#schedule_comission > tbody > tr").each(function () {
+            var day = $(this).data("day");
+            var starthour = $(this).data("starthour");
+            var endhour = $(this).data("endhour");
+            $("#formPost").prepend("<input type='hidden' name='" + listName + "[" + i + "].Day' value='" + day + "'>");
+            $("#formPost").prepend("<input type='hidden' name='" + listName + "[" + i + "].StartHour' value='" + starthour + "'>");
+            $("#formPost").prepend("<input type='hidden' name='" + listName + "[" + i + "].EndHour' value='" + endhour + "'>");
+            i = i+1;
+        });
+    });
+});
+
+//Function validate the correct structure of StartHour and EndHour
+function validar_hora_inicio_hora_fin()
+{
+    
+}
+
+var contador = 2;
 function obtener_dias() {
     var table = document.getElementById("schedule_comission");
     //var row_count = table.rows.length
     //alert(contador);
-    if (contador < 6) {
+    if (contador < 11) {
         var row = table.insertRow(contador);
         row.setAttribute("id", "trRow" + contador);
         row.setAttribute("data-day", "Lunes");
@@ -15,89 +40,241 @@ function obtener_dias() {
         
         var celda_StarHour = row.insertCell(1);
         var celda_EndHour = row.insertCell(2);
-        celda_dia.innerHTML = "<div class=\"campo_dia\">" +
-
-                              "<select id=\"tdRow"+contador+"Day\">" +
-                                  "<option><a href=\"#\">Lunes</a></option>" +
-                                  "<option><a href=\"#\">Martes</a></option>" +
-                                  "<option><a href=\"#\">Miercoles</a></option>" +
-                                  "<option><a href=\"#\">Jueves</a></option>" +
+        celda_Day.innerHTML = "<select id=\"tdRow" + (contador - 1) + "Day\" onchange=\"onchangeDaySelection("+(contador - 1) +")\">" +
+                                  "<option>Lunes</option>" +
+                                  "<option>Martes</option>" +
+                                  "<option>Miercoles</option>" +
+                                  "<option>Jueves</a></option>" +
                                   "<option><a href=\"#\">Viernes</a></option>" +
                                  " <option><a href=\"#\">Sabado</a></option>" +
-                              "</select>" +
-
-                          "</div>";
-        celda_StarHour.innerHTML = "<div class=\"campo_inicio\">" +
-
-                              "<select id=\"tdRow" + contador + "StartHour\">"+
-                                  "<option>7:30 AM</option>" +
-                                  "<option>8:30 AM</option>" +
-                                  "<option>9:30 AM</option>" +
-                                  "<option><a href=\"#\">10:30 am</a></option>" +
-                                  "<option><a href=\"#\">11:30 am</a></option>" +
-                                  "<option><a href=\"#\">12:30 pm</a></option>" +
-                                  "<option><a href=\"#\">1:00 pm</a></option>"+
-                                  "<option><a href=\"#\">2:00 pm</a></option>"+
-                                  "<option><a href=\"#\">3:00 pm</a></option>"+
-                                  "<option><a href=\"#\">4:00 pm</a></option>"+
-                                  "<option><a href=\"#\">5:00 pm</a></option>"+
-                                  "<option><a href=\"#\">6:00 pm</a></option>"+
-                                  "<option><a href=\"#\">7:00 pm</a></option>"+
-                                  "<option><a href=\"#\">8:00 pm</a></option>"+
-                                  "<option><a href=\"#\">9:00 pm</a></option>"+
-                              "</select>" +
-
-                          "</div>";
-        celda_EndHour.innerHTML = "<select id=\"tdRow" + contador + "EndHour\">"
-                                  "<option>8:20 AM</option>" +
-                                  "<option>9:20 AM</option>" +
-                                  "<option>10:20 AM</option>" +
-                                  "<option>11:20 AM</option>" +
-                                  "<option>12:20 PM</option>" +
-                                  "<option>1:50 PM</option>" +
-                                  "<option>2:50 PM</option>" +
-                                  "<option>3:50 PM</option>" +
-                                  "<option>4:50 PM</option>" +
-                                  "<option>5:50 PM</option>" +
-                                  "<option>6:50 PM</option>" +
-                                  "<option>7:50 PM</option>" +
-                                  "<option>8:50 PM</option>" +
-                                  "<option>9:50 PM</option>" +
                               "</select>";
+        celda_StarHour.innerHTML = "<select id=\"tdRow" + (contador - 1) + "StartHour\" onchange=\"onchangeStartHourSelection(" + (contador - 1) + ")\">" +
+                                  "<option>07:30 am</option>" +
+                                  "<option>08:30 am</option>" +
+                                  "<option>09:30 am</option>" +
+                                  "<option>10:30 am</option>" +
+                                  "<option>11:30 am</option>" +
+                                  "<option>12:30 pm</option>" +
+                                  "<option>01:00 pm</option>"+
+                                  "<option>02:00 pm</option>"+
+                                  "<option>03:00 pm</option>"+
+                                  "<option>04:00 pm</option>"+
+                                  "<option>05:00 pm</option>"+
+                                  "<option>06:00 pm</option>"+
+                                  "<option>07:00 pm</option>"+
+                                  "<option>08:00 pm</option>"+
+                                  "<option>09:00 pm</option>"+
+                              "</select>";
+        celda_EndHour.innerHTML = "<select id=\"tdRow" + (contador - 1) + "EndHour\" onchange=\"onchangeEndHourSelection(" + (contador - 1) + ")\">" +
+                                      "<option>08:20 am</option>" +
+                                      "<option>09:20 am</option>" +
+                                      "<option>10:20 am</option>" +
+                                      "<option>11:20 am</option>" +
+                                      "<option>12:20 pm</option>" +
+                                      "<option>01:50 pm</option>" +
+                                      "<option>02:50 pm</option>" +
+                                      "<option>03:50 pm</option>" +
+                                      "<option>04:50 pm</option>" +
+                                      "<option>05:50 pm</option>" +
+                                      "<option>06:50 pm</option>" +
+                                      "<option>07:50 pm</option>" +
+                                      "<option>08:50 pm</option>" +
+                                      "<option>09:50 pm</option>" +
+                                    "</select>";
     }
 
 
 }
 
-$('#tdRow1Day').change(function () 
+
+function onchangeDaySelection(identificador)
 {
-    $('#trRow1').attr('data-day', $('#tdRow1Day').val());
+    $('#trRow'+identificador).attr('data-day', $('#tdRow'+identificador+'Day').val());
 
-})
+}
 
+function onchangeStartHourSelection(identificador)
+{
+    var pStartHour = $('#tdRow' + identificador + 'StartHour').val();
+    $('#trRow'+identificador).attr('data-starthour',pStartHour);
+}
 
-$("#tdRow1StartHour").change(function () {
-    $('#trRow1').attr('data-starthour', $('#tdRow1StartHour').val());
-})
-
-$("#tdRow1EndHour").change(function () {
-    $('#trRow1').attr('data-endhour', $('#tdRow1EndHour').val());
-})
-
-
-$('#tdRow2Day').change(function () {
-    $('#trRow2').attr('data-day', $('#tdRow2Day').val());
-
-})
+function onchangeEndHourSelection(identificador) {
+    var vStartHour = $('#tdRow' + identificador + 'StartHour').val();
+    var vEndHour = $('#tdRow' + identificador + 'EndHour').val();
 
 
-$("#tdRow2StartHour").change(function () {
-    $('#trRow2').attr('data-starthour', $('#tdRow2StartHour').val());
-})
+    var int_hora_entrada;
+    var int_hora_salida;
+    switch (vStartHour)
+    {
+        case "07:30 am":
+            int_hora_entrada = 730;
+            break;
+        case "08:30 am":
+            int_hora_entrada = 830;
+            break;
+        case "09:30 am":
+            int_hora_entrada = 930;
+            break;
+        case "10:30 am":
+            int_hora_entrada = 1030;
+            break;
+        case "11:30 am":
+            int_hora_entrada = 1130;
+            break;
+        case "12:30 pm":
+            int_hora_entrada = 1230;
+            break;
 
-$("#tdRow2EndHour").change(function () {
-    $('#trRow2').attr('data-endhour', $('#tdRow2EndHour').val());
-})
+        case "01:00 pm":
+            int_hora_entrada = 1300;
+            break;
+        case "02:00 pm":
+            int_hora_entrada = 1400;
+            break;
+
+        case "03:00 pm":
+            int_hora_entrada = 1500;
+            break;
+        case "04:00 pm":
+            int_hora_entrada = 1600;
+            break;
+
+        case "05:00 pm":
+            int_hora_entrada = 1700;
+            break;
+        case "06:00 pm":
+            int_hora_entrada = 1800;
+            break;
+
+        case "07:00 pm":
+            int_hora_entrada = 1900;
+            break;
+        case "08:00 pm":
+            int_hora_entrada = 2000;
+            break;
+
+        case "09:00 pm":
+            int_hora_entrada = 2100;
+            break;
+    }
+
+    switch (vEndHour)
+    {
+        case "07:30 am":
+            int_hora_entrada = 730;
+            break;
+        case "08:30 am":
+            int_hora_entrada = 830;
+            break;
+        case "09:30 am":
+            int_hora_entrada = 930;
+            break;
+        case "10:30 am":
+            int_hora_entrada = 1030;
+            break;
+        case "11:30 am":
+            int_hora_entrada = 1130;
+            break;
+        case "12:30 pm":
+            int_hora_entrada = 1230;
+            break;
+
+        case "01:00 pm":
+            int_hora_entrada = 1300;
+            break;
+        case "02:00 pm":
+            int_hora_entrada = 1400;
+            break;
+
+        case "03:00 pm":
+            int_hora_entrada = 1500;
+            break;
+        case "04:00 pm":
+            int_hora_entrada = 1600;
+            break;
+
+        case "05:00 pm":
+            int_hora_entrada = 1700;
+            break;
+        case "06:00 pm":
+            int_hora_entrada = 1800;
+            break;
+
+        case "07:00 pm":
+            int_hora_entrada = 1900;
+            break;
+        case "08:00 pm":
+            int_hora_entrada = 2000;
+            break;
+
+        case "09:00 pm":
+            int_hora_entrada = 2100;
+            break;
+    }
+
+    switch (vEndHour)
+    {
+        case "08:20 am":
+            int_hora_salida = 820;
+            break;
+        case "09:20 am":
+            int_hora_salida = 920;
+            break;
+        case "10:20 am":
+            int_hora_salida = 1020;
+            break;
+        case "11:20 am":
+            int_hora_salida = 1120;
+            break;
+        case "12:20 pm":
+            int_hora_salida = 1220;
+            break;
+
+        case "01:50 pm":
+            int_hora_salida = 1350;
+            break;
+        case "02:50 pm":
+            int_hora_salida = 1450;
+            break;
+
+        case "03:50 pm":
+            int_hora_salida = 1550;
+            break;
+        case "04:50 pm":
+            int_hora_salida = 1650;
+            break;
+
+        case "05:50 pm":
+            int_hora_salida = 1750;
+            break;
+        case "06:50 pm":
+            int_hora_salida = 1850;
+            break;
+
+        case "07:50 pm":
+            int_hora_salida = 1950;
+            break;
+        case "08:50 pm":
+            int_hora_salida = 2050;
+            break;
+
+        case "09:50 pm":
+            int_hora_salida = 2150;
+            break;
+    }
+
+    //********************************************************************************
+    if (int_hora_salida > int_hora_entrada) {
+        $('#trRow' + identificador).attr('data-endhour', vEndHour);
+    }
+    else
+    {
+        alert("Error");
+    }
+   
+}
 /*
 $("#buttonPost").on("click", function () {
     var listName = "ScheduleComission";
