1f014f41c785625257b39c39a11cbde59103c19b
Cristian Araya <cristian.araya@hotmail.com>
2015-09-19 19:52:13 -0600

    OfertaAcademica Fix_v1

--- SACAAE/Controllers/BloqueXPlanController.cs
+++ SACAAE/Controllers/BloqueXPlanController.cs
@@ -1,32 +1,31 @@
-﻿/*
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SACAAE.Data_Access;
+using SACAAE.Models;
+using SACAAE.Models.ViewModels;
 
 namespace SACAAE.Controllers
 {
     public class BloqueXPlanController : Controller
     {
-        private RepositorioPlanesDeEstudio vRepoPlanes = new RepositorioPlanesDeEstudio();
-        private RepositorioBloqueAcademico vRepoBloques = new RepositorioBloqueAcademico();
-        private RepositorioBloqueXPlan vRepoBloquesXPlan = new RepositorioBloqueXPlan();
+        private SACAAEContext gvDatabase = new SACAAEContext();
         private const string TempDataMessageKey = "MessageError";
         private const string TempDataMessageKeySuccess = "MessageSuccess";
         // GET: BloqueXPlan
         [Authorize]
         public ActionResult CrearBloqueXPlan(int plan)
         {
-            var vBloqueXPlan = new BloqueAcademicoXPlanDeEstudio();
-            ViewBag.Planes = vRepoPlanes.ObtenerUnPlanDeEstudio(plan);
-            ViewBag.Bloques = vRepoBloques.ListarBloquesAcademicos();
+            ViewBag.Planes = ObtenerUnPlanDeEstudio(plan);
+            ViewBag.Bloques = ListarBloquesAcademicos();
             return View();
         }
 
         [Authorize]
         [HttpPost]
-        public ActionResult CrearBloqueXPlan(string button, BloqueAcademicoXPlanDeEstudio pBloqueXPlan, string selectPlanDeEstudio, string selectBloqueAcademico)
+        public ActionResult CrearBloqueXPlan(string button, AcademicBlockXStudyPlan pBloqueXPlan, string selectPlanDeEstudio, string selectBloqueAcademico)
         {
             int PlanID = Int16.Parse(selectPlanDeEstudio);
             if (button == "Asignar Curso")
@@ -36,13 +35,13 @@ namespace SACAAE.Controllers
             {
                 int BloqueID = Int16.Parse(selectBloqueAcademico);
                 pBloqueXPlan.PlanID = PlanID;
-                pBloqueXPlan.BloqueID = BloqueID;
-                if (vRepoBloquesXPlan.existeRelacionBloqueXPlan(pBloqueXPlan.PlanID, pBloqueXPlan.BloqueID))
+                pBloqueXPlan.BlockID = BloqueID;
+                if (existeRelacionBloqueXPlan(pBloqueXPlan.PlanID, pBloqueXPlan.BlockID))
                 {
                     TempData[TempDataMessageKey] = "Este plan ya cuenta con el bloque seleccionado. Por Favor intente de nuevo.";
                     return RedirectToAction("CrearBloqueXPlan", new { plan = PlanID });
                 }
-                vRepoBloquesXPlan.crearRelacionBloqueXPlan(pBloqueXPlan);
+                crearRelacionBloqueXPlan(pBloqueXPlan);
                 TempData[TempDataMessageKeySuccess] = "El bloque ha sido asignado al plan de estudio exitosamente";
                 return RedirectToAction("CrearBloqueXPlan", new { plan = PlanID });
 
@@ -51,9 +50,11 @@ namespace SACAAE.Controllers
             return RedirectToAction("CrearBloqueXPlan", new { plan = PlanID });
         }
 
+
+        [Route("BloqueXPlan/Bloques/List/{plan:int}")]
         public ActionResult ObtenerBloques(int plan)
         {
-            IQueryable listaBloques = vRepoBloques.ListarBloquesXPlan(plan);
+            IQueryable listaBloques = ListarBloquesXPlan(plan);
             if (HttpContext.Request.IsAjaxRequest())
             {
                 return Json(new SelectList(
@@ -64,6 +65,42 @@ namespace SACAAE.Controllers
             }
             return View(listaBloques);
         }
+
+        public StudyPlan ObtenerUnPlanDeEstudio(int plan)
+        {
+            return (from PlanesDeEstudio in gvDatabase.StudyPlans
+                    where PlanesDeEstudio.ID == plan
+                    select PlanesDeEstudio).FirstOrDefault(); ;
+        }
+
+        public IQueryable<AcademicBlock> ListarBloquesAcademicos()
+        {
+            return from BloquesAcademicos in gvDatabase.AcademicBlocks
+                   select BloquesAcademicos;
+        }
+
+        public IQueryable<AcademicBlock> ListarBloquesXPlan(int pPlanID)
+        {
+            return from Bloques in gvDatabase.AcademicBlocks
+                   join BloquesXPlan in gvDatabase.AcademicBlocksXStudyPlans on Bloques.ID equals BloquesXPlan.BlockID
+                   where BloquesXPlan.PlanID == pPlanID
+                   select Bloques;
+        }
+
+        public bool existeRelacionBloqueXPlan(int pPlanID, int pBloqueID)
+        {
+            return (gvDatabase.AcademicBlocksXStudyPlans.SingleOrDefault(relacion => relacion.PlanID == pPlanID && relacion.BlockID == pBloqueID) != null);
+        }
+
+        public void crearRelacionBloqueXPlan(AcademicBlockXStudyPlan pBloqueXPlan)
+        {
+            if (existeRelacionBloqueXPlan(pBloqueXPlan.PlanID, pBloqueXPlan.BlockID))
+                return;
+            else
+            {
+                gvDatabase.AcademicBlocksXStudyPlans.Add(pBloqueXPlan);
+                gvDatabase.SaveChanges();
+            }
+        }
     }
 }
-*/
