9f85eb2d90b1abac0eb9f83a1ee39fe649068ea0
Adonis <sinoda2991@gmail.com>
2015-10-12 20:00:44 -0600

    alerts

---
+++ SACAAE/Models/ViewModels/AlertViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SACAAE.Models.ViewModels
+{
+    public class AlertViewModel
+    {
+        public List<Commission> Commissions { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
